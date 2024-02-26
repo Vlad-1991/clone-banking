33d987dfb221f423b313f729381e04102b0805c2
Vlad-1991 <51714352+Vlad-1991@users.noreply.github.com>
2024-02-26 13:47:34 +0200

    Add files via upload

---
+++ src/components/ui/AppStatus.vue
@@ -0,0 +1,54 @@
+<template>
+  <span :class="['badge', className]">{{ text }}</span>
+</template>
+
+<script>
+import {ref, watch} from "vue";
+
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+      validator(value){
+        return ['active', 'cancelled', 'done', 'pending'].includes(value)
+      }
+    }
+  },
+  setup(props){
+
+
+    const classesMap = {
+      active: 'primary',
+      cancelled: 'danger',
+      done: 'primary',
+      pending: 'warning'
+    }
+
+    const textMap = {
+      active: 'Активен',
+      cancelled: 'Отменен',
+      done: 'Завершен',
+      pending: 'Выполняется'
+    }
+
+    watch(props, value => {
+      className.value = classesMap[value.type]
+      text.value = textMap[value.type]
+    })
+
+    const className = ref(classesMap[props.type])
+    const text = ref(textMap[props.type])
+
+
+    return {
+      className,
+      text
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

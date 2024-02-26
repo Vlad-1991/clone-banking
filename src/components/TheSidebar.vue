93e844793898defa12ddc2624b99a8922564fdf2
Vlad-1991 <51714352+Vlad-1991@users.noreply.github.com>
2024-02-26 13:40:03 +0200

    Add files via upload

---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="sidebar" v-if="sidebar">
+      <span class="sidebar-close" @click="close">&times;</span>
+
+      <div class="sidebar-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut cumque deserunt ea ex porro, veritatis?</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {useStore} from "vuex";
+import {computed} from "vue";
+
+export default {
+  setup(){
+    const store = useStore()
+
+    const sidebar = computed(() => store.state.sidebar)
+
+    return {
+      sidebar,
+      close: () => store.commit('closeSideBar')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

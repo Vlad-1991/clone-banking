a77d70e28f182432ae92bb239df4f7f0f7e87fc4
Vlad-1991 <51714352+Vlad-1991@users.noreply.github.com>
2024-02-26 13:38:36 +0200

    Add files via upload

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <the-navbar></the-navbar>
+  <the-sidebar></the-sidebar>
+  <div class="container with-nav">
+    <app-message></app-message>
+   <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import TheNavbar from "@/components/TheNavbar.vue";
+import AppMessage from "@/components/ui/AppMessage.vue";
+import TheSidebar from "@/components/TheSidebar.vue";
+export default {
+components: {TheNavbar, AppMessage, TheSidebar}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

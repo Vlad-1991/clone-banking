78072e3c3f153309f23cb3c49752fe27822c96cc
Vlad-1991 <51714352+Vlad-1991@users.noreply.github.com>
2024-02-26 13:51:44 +0200

    Add files via upload

---
+++ src/views/Help.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <app-page title="Помощь" back>
+      <question-item
+          v-for="question in questions"
+          :key="question.title"
+          :question="question"
+      ></question-item>
+    </app-page>
+    <question-item :question="questions"></question-item>
+  </div>
+</template>
+
+<script>
+import AppPage from "@/components/ui/AppPage.vue";
+import QuestionItem from "@/components/question/QuestionItem.vue";
+export default {
+  setup(){
+    const questions = [
+      {title: 'Что есть в данной системе?', text: 'Тут есть все чтобы смотреть заявки'},
+      {title: 'Как тут работает авторизация?', text: 'Все реализовано на Firebase'}
+    ]
+    return {questions}
+  },
+  components: {QuestionItem, AppPage}
+}
+</script>

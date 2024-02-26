c32ca2c21f824a95f5d8b051066359ba322c0af2
Vlad-1991 <51714352+Vlad-1991@users.noreply.github.com>
2024-02-26 13:37:55 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<component :is="layout + '-layout'" v-if="layout"></component>
+</template>
+
+<script>
+import {computed} from "vue";
+import AuthLayout from "@/layout/AuthLayout.vue";
+import MainLayout from "@/layout/MainLayout.vue";
+import {useRoute} from "vue-router";
+
+export default {
+    setup(){
+      const route = useRoute()
+
+     return {
+        layout: computed(() => route.meta.layout)
+     }
+    },
+    components: {MainLayout, AuthLayout}
+  }
+</script>
